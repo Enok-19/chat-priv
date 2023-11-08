bb92dd14a7392e7633cb98e006839a503aff6b14
Enok-19 <138811131+Enok-19@users.noreply.github.com>
2023-11-07 23:37:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var password = "tucontraseña"; // Cambia "tucontraseña" a la contraseña que deseas utilizar
+var authenticated = false;
 
 function authenticate() {
     var enteredPassword = document.getElementById('password').value;
     if (enteredPassword === password) {
+        authenticated = true;
         document.getElementById('login-form').style.display = 'none';
         document.getElementById('chat-box').style.display = 'block';
         document.getElementById('message-input').style.display = 'block';
@@ -12,6 +14,15 @@ function authenticate() {
     }
 }
 
-function sendMessage() {
-    // Función para enviar mensajes (código previo)
+function sendMessage(event) {
+    if (authenticated && event.key === "Enter") {
+        var messageInput = document.getElementById('message-input');
+        var message = messageInput.value.trim();
+        if (message !== '') {
+            var chatBox = document.getElementById('chat-box');
+            chatBox.innerHTML += '<p><strong>Tú:</strong> ' + message + '</p>';
+            chatBox.scrollTop = chatBox.scrollHeight;
+            messageInput.value = '';
+        }
+    }
 }
